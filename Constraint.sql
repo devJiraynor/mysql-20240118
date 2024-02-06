a1726a267445127d726abb22214a469dd847439d
devJiraynor <jiraynor@naver.com>
2024-02-06 15:50:25 +0900

    2024.02.06. 15:50

--- Constraint.sql
+++ Constraint.sql
@@ -47,6 +47,21 @@ CREATE TABLE primary_table (
     nomal_column INT
 );
 
+CREATE TABLE composite_table (
+    primary1 INT PRIMARY KEY,
+    primary2 INT PRIMARY KEY
+);
+
+CREATE TABLE composite_table (
+    -- primary1 INT PRIMARY KEY,
+    -- primary2 INT PRIMARY KEY
+    primary1 INT,
+    primary2 INT,
+    CONSTRAINT primary_key PRIMARY KEY (primary1, primary2)
+);
+
+SELECT * FROM information_schema.table_constraints;
+
 -- PRIMARY KEY 제약조건은 INSERT시에 NOT NULL의 조건과 UNIQUE의 조건을 만족해야함
 INSERT INTO primary_table VALUES (null, null);
 INSERT INTO primary_table VALUES (1, null);
@@ -56,7 +71,38 @@ UPDATE primary_table SET primary_column = null;
 UPDATE primary_table SET primary_column = 2;
 
 
+-- FOREIGN KEY: 특정 컬럼을 다른 테이블 혹은 같은 테이블의 기본키 컬럼과 연결
+CREATE TABLE foreign_table (
+    primary_column INT PRIMARY KEY,
+    foreign_column INT,
+    CONSTRAINT foreign_key FOREIGN KEY (foreign_column)
+    REFERENCES primary_table (primary_column)
+);
+-- FOREIGN KEY 제약조건이 걸린 컬럼에 INSERT 작업시 참조하고 있는 테이블의 컬럼에 값이 존재하지 않으면 지정 할 수 없음
+INSERT INTO foreign_table VALUES (1, 1);
+INSERT INTO foreign_table VALUES (2, 3);
+-- FOREIGN KEY 제약조건이 걸린 컬럼에 UPDATE 작업시 참조하고 있는 테이블의 컬럼에 값이 존재하지 않으면 지정 할 수 없음
+UPDATE foreign_table SET foreign_column = 3;
 
+-- 특정 테이블에서 기본키를 참조하고 있는 레코드가 존재한다면 해당 레코드를 삭제하지 못함
+DELETE FROM primary_table WHERE primary_column = 1;
 
+DELETE FROM foreign_table;
+
+-- 특정 테이블을 참조하고 있는 테이블이 존재한다면 테이블 구조를 제거할 수 없음
+DROP TABLE primary_table;
+
+-- 특정 테이블에서 기본키를 참조하고 있는 레코드가 존재한다면 해당 레코드를 수정하지 못함
+UPDATE primary_table 
+SET primary_column = 3 
+WHERE primary_column = 1;
+
+-- ON UPDATE / ON DELETE 옵션
+-- ON UPDATE: 참조하고 있는 테이블의 기본키가 변경되었을 때 동작
+-- ON DELETE: 참조하고 있는 테이블의 기본키가 삭제되었을 때 동작
+
+-- CASCADE : 참조하고 있는 테이블에서 데이터를 삭제하거나 수정했을 때, 참조하는 테이블에서도 삭제와 수정이 같이 일어남
+-- SET NULL : 참조하고 있는 테이블에서 데이터를 삭제하거나 수정했을 때, 참조하는 테이블의 해당 데이터를 null로 지정
+-- RESTRICT : 참조하는 테이블에 참조하는 데이터가 존재한다면 수정, 삭제가 불가능
 
 
