da730848988ef8fca612531dfa11d2a85104a831
devJiraynor <jiraynor@naver.com>
2024-02-13 10:51:01 +0900

    2024.02.13. 10:50

--- Transaction.sql
+++ Transaction.sql
@@ -2,6 +2,12 @@ USE practice_sql;
 
 -- Transaction: SQL로 작업하는 작업 단위
 
+-- Transaction의 4가지 특성
+-- Atomicity (원자성) : 트랜잭션은 모두 성공하거나 모두 실패해야 한다.
+-- Consistency (일관성) : 트랜잭션의 작업 결과는 항상 일관성이 있어야 한다.
+-- Isolation (독립성) : 트랜잭션은 모두 독립적이어야 한다.
+-- Durablitiy (영구성) : 트랜잭션이 성공적으로 완료되면 영구히 반영되어야 한다.
+
 -- 회원가입: user - email_authentication
 -- 사용자는 아이디를 입력하고 중복 확인을 함
 -- 만약 입력한 아이디가 종복된 아이디가 아니라면
@@ -32,3 +38,62 @@ SELECT * FROM user WHERE id = 'idddd';
 
 -- 2. 이메일 인증
 INSERT email_authentication VALUES ('email@email.com', 'OCMD');
+
+-- 서버가 지정한 이메일로 인증코드를 전송하고 사용자는 그 코드를 확인한 후 인증코드를 입력하여 인증함
+
+-- 3. 이메일 인증 확인
+SELECT * FROM email_authentication 
+WHERE email = 'email@email.com' AND authentication_code = 'OCMD';
+
+-- 4. 회원가입 처리
+INSERT user VALUES ('idddd', 'P!ssw0rd', 'email@email.com');
+
+SHOW variables like '%commit%';
+SET AUTOCOMMIT = 0;
+
+-- 트랜잭션 시작
+START TRANSACTION;
+
+-- 사용자가 아이디를 'idddd'로 입력했다.
+
+-- 1. 아이디 중복확인
+SELECT * FROM user WHERE id = 'idddd3';
+
+-- 사용자가 비밀번호를 'P!ssw0rd', 비밀번호 확인을 'P!ssw0rd',
+-- 이메일을 'email@email.com', 서버가 생성한 코드는 'OCMD'이다.
+
+-- 2. 이메일 인증
+INSERT email_authentication VALUES ('email4@email.com', 'OCMD');
+
+-- 트랜잭션 초기상태로 변경 (트랜잭션 취소)
+ROLLBACK;
+
+-- 트랜잭션 롤백 위치 지정
+SAVEPOINT A;
+
+-- 서버가 지정한 이메일로 인증코드를 전송하고 사용자는 그 코드를 확인한 후 인증코드를 입력하여 인증함
+
+-- 3. 이메일 인증 확인
+SELECT * FROM email_authentication 
+WHERE email = 'email3@email.com' AND authentication_code = 'OCMD';
+
+-- 4. 회원가입 처리
+INSERT user VALUES ('idddd4', 'P!ssw0rd', 'email4@email.com');
+
+-- 특정 세이브포인트로 롤백
+ROLLBACK TO SAVEPOINT A;
+
+-- 트랜잭션 성공 처리 (영구히 적용)
+COMMIT;
+
+SELECT * FROM user;
+SELECT * FROM email_authentication;
+
+CREATE USER 'student'@'%' identified by 'qwer1234';
+GRANT SELECT ON *.* TO 'student'@'%';
+
+-- LIMIT 개수 : 결과테이블에서 지정한 개수만큼의 상위 데이터만 보여줌
+SELECT  * FROM jeju LIMIT 10;
+-- LIMIT 재외 레코드 개수, 개수 : 상위에서 재외 레코드 개수만큼 재외 후 개수만큼의 데이터만 보여줌
+SELECT * FROM jeju LIMIT 10, 3;
+
